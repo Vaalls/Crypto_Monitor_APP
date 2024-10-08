fe3b155cecab87d3dba5f451a43ca1c03c3b6c93
Vaalls <bielvalls4@gmail.com>
2024-10-07 21:43:17 -0300

    Limpando códigos não necessários da classe MainActivity.kt

--- app/src/main/java/vaalls/com/github/crypto_monitor_app/MainActivity.kt
+++ app/src/main/java/vaalls/com/github/crypto_monitor_app/MainActivity.kt
@@ -15,32 +15,6 @@ import vaalls.com.github.crypto_monitor_app.ui.theme.Crypto_Monitor_APPTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Crypto_Monitor_APPTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Crypto_Monitor_APPTheme {
-        Greeting("Android")
     }
 }
