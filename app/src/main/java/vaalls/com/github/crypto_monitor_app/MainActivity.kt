6347441f679814c06268097a34955af65dadb4ac
Vaalls <bielvalls4@gmail.com>
2024-10-07 22:02:24 -0300

    Configurando a MainActivity.kt para executar com o layout pronto.

--- app/src/main/java/vaalls/com/github/crypto_monitor_app/MainActivity.kt
+++ app/src/main/java/vaalls/com/github/crypto_monitor_app/MainActivity.kt
@@ -2,19 +2,25 @@ package vaalls.com.github.crypto_monitor_app
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import vaalls.com.github.crypto_monitor_app.ui.theme.Crypto_Monitor_APPTheme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    class MainActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
 
+            // Configurando a toolbar
+            val toolbarMain: Toolbar = findViewById(R.id.toolbar_main)
+            configureToolbar(toolbarMain)
+        }
+
+        private fun configureToolbar(toolbar: Toolbar) {
+            setSupportActionBar(toolbar)
+            toolbar.setTitleTextColor(getColor(R.color.white))
+            supportActionBar?.setTitle(getText(R.string.app_title))
+            supportActionBar?.setBackgroundDrawable(getDrawable(R.color.primary))
+        }
     }
 }
